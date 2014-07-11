5ddcb538f4f894f0b940e5c767632137b2d1aad1
Ruan Pearce-Authers <ruanpa@outlook.com>
2014-07-11 19:32:28 +0100

    Demonstrate non-string params

--- Samples/HelloWorld/Program.cs
+++ Samples/HelloWorld/Program.cs
@@ -14,12 +14,12 @@ namespace HelloWorld
 			return "Hello there, " + name;
 		}
 
-		// Available as: http://host/program/belatedhello?name=whatever
-		// The response will take ~2s to come through, but this won't block server resources
+		// Available as: http://host/program/belatedhello?name=whatever&delay=2
+		// The response will take <delay> seconds to come through, but this won't block server resources
 		[Route]
-		public async Task<string> BelatedHello(string name)
+		public async Task<string> BelatedHello(string name, int delay)
 		{
-			await Task.Delay(TimeSpan.FromSeconds(2));
+			await Task.Delay(TimeSpan.FromSeconds(delay));
 			return "Apologies for keeping you waiting, " + name;
 		}
 
