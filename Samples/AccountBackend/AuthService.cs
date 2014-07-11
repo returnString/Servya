86f0c42b85e126f44dcd095fb7d23be1dac0f3ee
Ruan Pearce-Authers <ruanpa@outlook.com>
2014-07-11 20:55:59 +0100

    Added AccountBackend sample

---
+++ Samples/AccountBackend/AuthService.cs
@@ -0,0 +1,79 @@
+﻿using Servya;
+using System.Security.Cryptography;
+using System.Threading.Tasks;
+using StackExchange.Redis;
+using System.Text;
+
+namespace AccountBackend
+{
+	public enum RegistrationStatus
+	{
+		Ok,
+		UnknownError,
+		NameTaken,
+	}
+
+	public enum AuthStatus
+	{
+		Ok,
+		Failed
+	}
+
+	[Service]
+	public class AuthService
+	{
+		private readonly IDatabase m_db;
+
+		public AuthService(IDatabase db)
+		{
+			m_db = db;
+		}
+
+		[Route]
+		public async Task<RegistrationStatus> Register(string name, string password)
+		{
+			var key = UserKey(name);
+
+			var transaction = m_db.CreateTransaction();
+			var nameNotTaken = transaction.AddCondition(Condition.KeyNotExists(key));
+			transaction.HashSetAsync(key, "password", PasswordHash(password)).Forget();
+
+			if (await transaction.ExecuteAsync())
+				return RegistrationStatus.Ok;
+
+			if (!nameNotTaken.WasSatisfied)
+				return RegistrationStatus.NameTaken;
+
+
+			return RegistrationStatus.UnknownError;
+		}
+
+		[Route]
+		public async Task<AuthStatus> Login(string name, string password)
+		{
+			var key = UserKey(name);
+
+			var storedPassword = await m_db.HashGetAsync(key, "password");
+
+			if (storedPassword == PasswordHash(password))
+				return AuthStatus.Ok;
+
+			return AuthStatus.Failed;
+		}
+
+		private byte[] PasswordHash(string password)
+		{
+			using (var alg = new SHA512Managed())
+			{
+				var bytes = Encoding.UTF8.GetBytes(password);
+				return alg.ComputeHash(bytes);
+			}
+		}
+
+		private string UserKey(string name)
+		{
+			return "user:" + name;
+		}
+	}
+}
+
---
+++ Samples/AccountBackend/Program.cs
@@ -0,0 +1,34 @@
+﻿using Servya;
+using StackExchange.Redis;
+
+namespace AccountBackend
+{
+	class Program
+	{
+		static void Main()
+		{
+			var host = new AccountBackendHost();
+			var config = new AccountBackendConfig();
+			config.Http = HttpConfig.DevDefault;
+			config.RedisServer = "127.0.0.1";
+
+			App.Run(host, config);
+		}
+	}
+
+	public class AccountBackendHost : Host<AccountBackendConfig> 
+	{
+		// Add our Redis connection to the dependency resolver
+		// This way, services can get it from their constructor
+		protected override void PreServiceInit()
+		{
+			var multiplexer = ConnectionMultiplexer.Connect(Config.RedisServer);
+			Resolver.Add(multiplexer.GetDatabase());
+		}
+	}
+
+	public class AccountBackendConfig : Config
+	{
+		public string RedisServer { get; set; }
+	}
+}
