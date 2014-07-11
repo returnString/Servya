e9dee7d0c4bd37f6661123c0faab1b836d2cca77
Ruan Pearce-Authers <ruanpa@outlook.com>
2014-07-11 22:27:47 +0100

    Indent JSON responses

--- Samples/AccountBackend/JsonRoute.cs
+++ Samples/AccountBackend/JsonRoute.cs
@@ -5,9 +5,14 @@ namespace AccountBackend
 {
 	public class JsonRouteAttribute : RouteAttribute
 	{
+		private static JsonSerializerSettings JsonSettings = new JsonSerializerSettings
+		{
+			Formatting = Formatting.Indented,
+		};
+
 		public override object Transform(object response)
 		{
-			return JsonConvert.SerializeObject(response);
+			return JsonConvert.SerializeObject(response, JsonSettings);
 		}
 
 		public override object HandleError(RouteError error)
