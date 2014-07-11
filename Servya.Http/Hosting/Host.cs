0811938bf40c35cf48062734d0d66a2ac5e7ab16
Ruan Pearce-Authers <ruanpa@outlook.com>
2014-07-11 20:55:21 +0100

    Added pre service init hook for custom Hosts

--- Servya.Http/Hosting/Host.cs
+++ Servya.Http/Hosting/Host.cs
@@ -6,7 +6,7 @@ namespace Servya
 	{
 		public static void DevRun()
 		{
-			App.Run(new Host(), Config.DevDefault);
+			App.Run(new Host<Config>(), Config.DevDefault);
 		}
 
 		public static void Run<THost, TConfig>(THost host, TConfig config)
@@ -25,13 +25,20 @@ namespace Servya
 	{
 		protected DependencyResolver Resolver { get; private set; }
 		protected Router Router { get; private set; }
+		protected TConfig Config { get; private set; }
 
 		public void Run(TConfig config)
 		{
+			Config = config;
 			Resolver = new DependencyResolver();
 			Router = new Router();
 
+			PreServiceInit();
 			InitHttp(config.Http);
+		}
+
+		protected virtual void PreServiceInit()
+		{
 		}
 
 		private void InitHttp(HttpConfig config)
@@ -42,10 +49,6 @@ namespace Servya
 			autoRouter.CreateWebInterface();
 			listener.Start(() => new EventLoopContext(), Environment.ProcessorCount);
 		}
-	}
-
-	public class Host : Host<Config>
-	{
 	}
 }
 
