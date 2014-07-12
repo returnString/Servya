7a3c35302085ccd6502f020c5c6a5fbeb1013fd5
Ruan Pearce-Authers <ruanpa@outlook.com>
2014-07-12 19:39:31 +0100

    Split App into separate file

---
+++ Servya.Http/Hosting/App.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Servya
+{
+	public static class App
+	{
+		public static void Run()
+		{
+			App.Run(new Host<Config>(), Config.DevDefault);
+		}
+
+		public static void Run<THost, TConfig>(THost host, TConfig config)
+			where THost : Host<TConfig>
+			where TConfig : Config
+		{
+			host.Run(config);
+
+			while (true)
+				Console.ReadLine();
+		}
+	}
+}
+
--- Servya.Http/Hosting/Host.cs
+++ Servya.Http/Hosting/Host.cs
@@ -2,24 +2,6 @@
 
 namespace Servya
 {
-	public static class App
-	{
-		public static void Run()
-		{
-			App.Run(new Host<Config>(), Config.DevDefault);
-		}
-
-		public static void Run<THost, TConfig>(THost host, TConfig config)
-			where THost : Host<TConfig>
-			where TConfig : Config
-		{
-			host.Run(config);
-
-			while (true)
-				Console.ReadLine();
-		}
-	}
-
 	public class Host<TConfig>
 		where TConfig : Config
 	{
